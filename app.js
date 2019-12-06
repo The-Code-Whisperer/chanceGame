f51179131b446f02be4547242a84f7795a296827
Steven Kim <hansomekim@gmail.com>
2019-12-06 18:04:49 -0500

    jpg

--- app.js
+++ app.js
@@ -23,9 +23,9 @@ document.querySelector('.btn-roll').addEventListener('click', function () {
         var dice2 = Math.floor(Math.random() * 6 + 1);
         // display result
         diceDOM.style.display = 'block';
-        diceDOM.src = 'dice-' + dice + '.png';
+        diceDOM.src = 'dice-' + dice + '.jpg';
         diceDOM2.style.display = 'block';
-        diceDOM2.src = 'dice-' + dice2 + '.png';
+        diceDOM2.src = 'dice-' + dice2 + '.jpg';
         // update the round score if rolled number not 1
         if (dice !== 1 && dice2 !== 1) {
             // add score to scoreround
